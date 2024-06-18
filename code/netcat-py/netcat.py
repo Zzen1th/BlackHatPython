95105636af5e8f72265664ec227a1d6866307cbf
zve <695162804@qq.com>
2024-06-19 01:00:48 +0800

    netcat-py-分析

--- code/netcat-py/netcat.py
+++ code/netcat-py/netcat.py
@@ -7,12 +7,19 @@ import subprocess
 import sys
 import textwrap
 import threading
+import os
 
 
 def execute(cmd):
     cmd = cmd.strip()
     if not cmd:
         return
+    if cmd.startswith("cd"):
+        try:
+            os.chdir(cmd.split(" ")[1])
+            return ""
+        except Exception as e:
+            return str(e)
     output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
 
     return output.decode()
@@ -20,6 +27,7 @@ def execute(cmd):
 
 class NetCat:
     def __init__(self, args, buffer=None) -> None:
+        print("init netcat")
         self.args = args
         self.buffer = buffer
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -116,10 +124,11 @@ if __name__ == "__main__":
     parser.add_argument("-c", "--command", action="store_true", help="command shell")
     parser.add_argument("-e", "--execute", help="execute specified command")
     parser.add_argument("-l", "--listen", action="store_true", help="listen")
-    parser.add_argument("-p", "--port", type=int, default=5555, help="specified port")
-    parser.add_argument("-t", "--target", default="192.168.1.203", help="specified IP")
+    parser.add_argument("-p", "--port", type=int, help="specified port")
+    parser.add_argument("-t", "--target", help="specified IP")
     parser.add_argument("-u", "--upload", help="upload file")
     args = parser.parse_args()
+    # debug
     if args.listen:
         buffer = ""
     else:
